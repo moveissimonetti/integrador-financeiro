eede94354d524b27766fe823e848b4486f28da5f
Ráfaga Queiroz <rafaga.queiroz@gmail.com>
2017-02-15 16:56:51 -0200

    Acertando definicao de constante

--- app/config.php
+++ app/config.php
@@ -10,6 +10,6 @@ define('DB_NAME', getenv('DB_NAME') ?: 'test');
 define('RABBIT_HOST', getenv('RABBIT_HOST') ?: 'rabbit');
 define('RABBIT_PORT', getenv('RABBIT_PORT') ?: 5672);
 define('RABBIT_USER', getenv('RABBIT_USER') ?: 'guest');
-define('RABBIT_PASSWORD', getenv('RABBIT_PASSWORD' ?: 'guest'));
+define('RABBIT_PASSWORD', getenv('RABBIT_PASSWORD') ?: 'guest');
 define('RABBIT_VHOST', getenv('RABBIT_VHOST') ?: '/');
 define('APP_ENV', getenv('APP_ENV') ?: 'dev');
