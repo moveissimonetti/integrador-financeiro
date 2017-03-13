86e52d63deb34bfebfb70ca8b2f0adc659b2416f
Ráfaga Queiroz <rafaga.queiroz@gmail.com>
2017-03-13 11:23:01 -0300

    Alterando script handler para parar execucao se nao houver arquivo composer

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -11,6 +11,10 @@ class ScriptHandler
     {
         $filename = __DIR__ . '/../app/composer.local.json';
 
+        if (!file_exists($filename)) {
+            return;
+        }
+
         $data = json_decode(file_get_contents($filename), true);
 
         $providers = [];
