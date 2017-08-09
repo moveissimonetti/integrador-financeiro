9bdcc8389fdde5b0b9a2d25cbf162bbcb62245d5
Carlos Rodrigues <carlosrodriguesf96@gmail.com>
2017-08-09 10:22:42 -0300

    Corrigindo Integrator Controller

--- app/routers.php
+++ app/routers.php
@@ -1,8 +1,8 @@
 <?php
 $app->mount('/api', function ($api) {
-    $api->post('/integrate/{sourceIdentifier}/{queryParameter}', 'integrate.controller:integrateAction');
+    $api->post('/integrate/{sourceIdentifier}/{queryParameter}', 'integrator.controller:integrateAction');
 
-    $api->get('/search/{sourceIdentifier}', 'integrate.controller:searchAction');
+    $api->get('/search/{sourceIdentifier}', 'integrator.controller:searchAction');
 
     $api->mount('/source', function ($source) {
         $source->get('/search', 'source.controller:searchAction');
